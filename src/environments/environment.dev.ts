78000bbb3a05744b8cf00a9a15c73a6cd7214300
Aratikatla Krishna Kundan <a.krishnakundan@gmail.com>
2024-01-21 20:16:32 +0530

    Untracked some files

--- src/environments/environment.dev.ts
+++
@@ -1,9 +0,0 @@
-import { Environment } from "./enviroment";
-
-export const DevEnvironment:Environment = {
-    db_uri: "mongodb+srv://akrishnakundan:Ammagari1Swiggy@cluster0.2t6pzwn.mongodb.net/SwiggyCloneApp?retryWrites=true&w=majority",
-    jwt_secret_key:'unknown secret key', 
-    appPassword: "lgxl epnd vqik kfai",
-    appEmail: "kavikris101@gmail.com"
-}
-
